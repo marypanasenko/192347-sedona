c9226689180f14a9cd1756722f2b9b7e2dc0a45a
Maria Panasenko <kerch.pm@gmail.com>
2017-10-19 20:29:51 +0300

    Add final Js

--- js/gmap.js
+++ js/gmap.js
@@ -1,12 +1,12 @@
 function initialize() {
   var mapOptions = {
     zoom: 7,
-    center: new google.maps.LatLng(34.8643986,-111.8141847)
+    center: new google.maps.LatLng(34.8692161,-111.7651565)
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'),
                                 mapOptions);
   var image = "img/icon-map-marker.svg";
-  var myLatLng = new google.maps.LatLng(34.8643986,-111.8141847);
+  var myLatLng = new google.maps.LatLng(34.8692161,-111.7651565);
   var beachMarker = new google.maps.Marker({
     position: myLatLng,
     map: map,
